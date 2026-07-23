30081223ee02deab4b548fb0857a972f40e81200
tanujisnowheretobeseen <141784626+tanujisnowheretobeseen@users.noreply.github.com>
2026-07-23 16:26:38 -0700

    API has been integrated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
+const apiKey = "e892968134cbb6b331801a5a68e787d8";
 console.log("JavaScript connected!");
 
 const searchButton = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
 
 searchButton.addEventListener("click", function () {
-
-    const city = cityInput.value;
+    const city = cityInput.value.trim();
 
     if (city === "") {
         alert("Please enter a valid city name.");
         return;
     }
 
-    console.log(city);
+    const apiUrl =
+        `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=imperial`;
 
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
 });
